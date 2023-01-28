0510a85dabb316afa5d02b3e5bfd14033dd4ac67
unknown <651765459@qq.com>
2023-01-28 17:25:08 +0800

    create love dir & file

---
+++ lover/js/wenzi.js
@@ -0,0 +1,14 @@
+/*
+ 
+  Shape Shifter
+  =============
+  A canvas experiment by Kenneth Cachia
+  http://www.kennethcachia.com
+ 
+  Updated code
+  ------------
+  https://github.com/kennethcachia/Shape-Shifter
+ 
+*/
+ 
+var wenzi = '\u0032\u0030\u0032\u0033\u007c\u795d\u007c\u4eb2\u007c\u7231\u007c\u7684\u007c\uff1a\u007c\u5218\u007c\u7f8e\u007c\u73b2\u007c\u7231\u007c\u4f34\u007c\u6c38\u007c\u8fdc\u007c\u007e\u007c\u6c38\u007c\u8fdc\u007c\u5e78\u007c\u798f\u007c\uff01' 
