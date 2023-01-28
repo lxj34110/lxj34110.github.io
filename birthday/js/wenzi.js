2a9fa8305b1148bdd4fd0280bcc56cfdb8eeefac
unknown <651765459@qq.com>
2023-01-28 16:49:28 +0800

    create new dir & file

---
+++ birthday/js/wenzi.js
@@ -0,0 +1,14 @@
+/*
+ 
+  Shape Shifter
+  =============
+  A canvas experiment by Kenneth Cachia
+  http://www.kennethcachia.com
+ 
+  Updated code
+  ------------
+  https://github.com/kennethcachia/Shape-Shifter
+ 
+*/
+ 
+var wenzi = '2023|祝|亲|爱|的|：|刘|美|玲|生|日|快|乐|~|永|远|幸|福|！' 
